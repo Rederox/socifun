1f12357705918f40a90bb3743d48fdfa3c6801ce
Theivathan Thevaraj <theivathan.thevaraj@chiffr3.com>
2023-10-24 20:42:31 +0200

    addSitemap

---
+++ next-sitemap-config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://socifun.theivathan.fr',
+    generateRobotsTxt: true, // (optional)
+    
+  }
